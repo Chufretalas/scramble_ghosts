8a87bb99bc76cb0f4adecde23a8b192c2b4190c0
Chufretalas <marcobene.contact@gmail.com>
2023-06-15 19:52:21 -0300

    the enemies can be killed

--- main.go
+++ main.go
@@ -19,17 +19,19 @@ const (
 )
 
 var (
-	toRemove []int
+	bulletsToRemove []int
 )
 
 type Enemy struct {
 	x, y          float32
 	width, height float32
 	hit           bool
+	alive         bool
 }
 
 type Bullet struct {
-	x, y float32
+	x, y          float32
+	width, height float32
 }
 
 type Game struct {
@@ -46,35 +48,45 @@ func (g *Game) Update() error {
 
 	// fire bullets
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
-		g.bullets = append(g.bullets, &Bullet{g.player.x + g.player.width/2, g.player.y})
+		g.bullets = append(g.bullets, &Bullet{g.player.x + g.player.width/2, g.player.y, 10, 10})
 	}
 
-	//check for player collisions
-	for _, enemy := range g.enemies {
-		if utils.IsColliding(enemy.x, enemy.y, enemy.width, enemy.height, g.player.x, g.player.y, g.player.width, g.player.height) {
-			enemy.hit = true
-		} else {
-			enemy.hit = false
-		}
-	}
-
-	// Remove bullets outr of frame
+	// move bullets
 	for i, bullet := range g.bullets {
 		bullet.y -= bV
 		if bullet.y+10 < 0 {
-			toRemove = append(toRemove, i)
+			bulletsToRemove = append(bulletsToRemove, i)
 		}
 	}
 
-	if len(toRemove) != 0 {
-		newBullets := make([]*Bullet, 0, len(g.bullets)-len(toRemove))
+	//check for collisions
+	for _, enemy := range g.enemies {
+		if enemy.alive {
+			for bullet_index, bullet := range g.bullets { //TODO: remove the -5 magic number once the bullets stop beign a weird circle
+				if utils.IsColliding(bullet.x-5, bullet.y, bullet.width, bullet.height, enemy.x, enemy.y, enemy.width, enemy.height) {
+					// enemy.hit = true
+					enemy.alive = false
+					bulletsToRemove = append(bulletsToRemove, bullet_index)
+					break
+				}
+			}
+			if utils.IsColliding(enemy.x, enemy.y, enemy.width, enemy.height, g.player.x, g.player.y, g.player.width, g.player.height) && enemy.alive {
+				enemy.hit = true
+			} else {
+				enemy.hit = false
+			}
+		}
+	}
+
+	if len(bulletsToRemove) != 0 {
+		newBullets := make([]*Bullet, 0, len(g.bullets)-len(bulletsToRemove))
 		for i, bullet := range g.bullets {
-			if !utils.InSlice(toRemove, i) {
+			if !utils.InSlice(bulletsToRemove, i) {
 				newBullets = append(newBullets, bullet)
 			}
 		}
 		g.bullets = newBullets
-		toRemove = make([]int, 0)
+		bulletsToRemove = make([]int, 0)
 	}
 
 	return nil
@@ -85,10 +97,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	vector.DrawFilledRect(screen, g.player.x, g.player.y, g.player.width, g.player.height, color.White, true)
 	var enemyColor color.Color
 	for _, enemy := range g.enemies {
-		if enemy.hit {
-			enemyColor = color.RGBA{100, 200, 100, 255}
+		if enemy.alive {
+			if enemy.hit {
+				enemyColor = color.RGBA{100, 200, 100, 255}
+			} else {
+				enemyColor = color.RGBA{255, 0, 0, 255}
+			}
 		} else {
-			enemyColor = color.RGBA{255, 0, 0, 255}
+			enemyColor = color.RGBA{200, 0, 0, 255}
 		}
 		vector.DrawFilledRect(screen,
 			enemy.x,
@@ -100,7 +116,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 
 	for _, bullet := range g.bullets {
-		vector.DrawFilledCircle(screen, bullet.x, bullet.y, 10, color.RGBA{255, 0, 0, 255}, true)
+		vector.DrawFilledCircle(screen, bullet.x, bullet.y, bullet.width, color.RGBA{255, 0, 0, 255}, true)
 	}
 }
 
@@ -114,10 +130,10 @@ func main() {
 		bullets: make([]*Bullet, 0),
 		player:  Player{x: 0, y: 0, width: 30, height: 30},
 	}
-	game.enemies = append(game.enemies, &Enemy{x: screenWidth / 2, y: screenHeight / 2, width: 30, height: 30, hit: false})
-	game.enemies = append(game.enemies, &Enemy{x: screenWidth/2 + 50, y: screenHeight / 2, width: 30, height: 30, hit: false})
-	game.enemies = append(game.enemies, &Enemy{x: screenWidth / 2, y: screenHeight/2 + 50, width: 30, height: 30, hit: false})
-	toRemove = make([]int, 0)
+	game.enemies = append(game.enemies, &Enemy{x: screenWidth / 2, y: screenHeight / 2, width: 30, height: 30, hit: false, alive: true})
+	game.enemies = append(game.enemies, &Enemy{x: screenWidth/2 + 50, y: screenHeight / 2, width: 30, height: 30, hit: false, alive: true})
+	game.enemies = append(game.enemies, &Enemy{x: screenWidth / 2, y: screenHeight/2 + 50, width: 30, height: 30, hit: false, alive: true})
+	bulletsToRemove = make([]int, 0)
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Scramble Ghosts 👻")
 	if err := ebiten.RunGame(game); err != nil {
