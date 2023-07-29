39aac2377748c9769bd68c5641c6b97700adc317
Chufretalas <marcobene.contact@gmail.com>
2023-07-29 16:57:26 -0300

    fixed a bug related to the deathwall spawn spilling into a new game

--- spawn_deathwall.go
+++ spawn_deathwall.go
@@ -13,28 +13,48 @@ func (g *Game) SpawnDeathWall(side string) {
 		g.DWL.IsSpawning = true
 		g.TimerSystem.After(time.Millisecond*800, func() {
 			g.ShowDWWL = false
-			g.TimerSystem.After(time.Millisecond*400, func() {
-				g.ShowDWWL = true
-				g.TimerSystem.After(time.Millisecond*800, func() {
-					g.ShowDWWL = false
-					g.DWL.IsSpawning = false
-					g.DWL.Active = true
+			if g.DWL.IsSpawning {
+				g.TimerSystem.After(time.Millisecond*400, func() {
+					g.ShowDWWL = true
+					if g.DWL.IsSpawning {
+						g.TimerSystem.After(time.Millisecond*800, func() {
+							g.ShowDWWL = false
+							if g.DWL.IsSpawning {
+								g.DWL.IsSpawning = false
+								g.DWL.Active = true
+							}
+						})
+					} else {
+						g.ShowDWWL = false
+					}
 				})
-			})
+			} else {
+				g.ShowDWWL = false
+			}
 		})
 	case "right":
 		g.ShowDWWR = true
 		g.DWR.IsSpawning = true
-		g.TimerSystem.After(time.Millisecond*800, func() {
-			g.ShowDWWR = false
-			g.TimerSystem.After(time.Millisecond*400, func() {
-				g.ShowDWWR = true
-				g.TimerSystem.After(time.Millisecond*800, func() {
+		if g.DWR.IsSpawning {
+			g.TimerSystem.After(time.Millisecond*800, func() {
+				g.ShowDWWR = false
+				if g.DWR.IsSpawning {
+					g.TimerSystem.After(time.Millisecond*400, func() {
+						g.ShowDWWR = true
+						g.TimerSystem.After(time.Millisecond*800, func() {
+							g.ShowDWWR = false
+							if g.DWR.IsSpawning {
+								g.DWR.IsSpawning = false
+								g.DWR.Active = true
+							}
+						})
+					})
+				} else {
 					g.ShowDWWR = false
-					g.DWR.IsSpawning = false
-					g.DWR.Active = true
-				})
+				}
 			})
-		})
+		} else {
+			g.ShowDWWR = false
+		}
 	}
 }
